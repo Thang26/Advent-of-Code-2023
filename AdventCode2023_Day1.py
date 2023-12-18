da51dcf5d943ac207697ea4371032e3f85658d6d
Thang Pham <thang10262001@gmail.com>
2023-12-18 04:34:47 -0500

    Finished solution for part 1 of day 1 puzzle.

---
+++ AdventCode2023_Day1.py
@@ -0,0 +1,39 @@
+def main():
+
+    indvCharList = []
+    sumList = []
+    a = 0
+    b = 0
+    total = 0
+
+    with open("Day1_Puzzle.txt", "r") as file:
+        file_list = file.readlines()
+
+        # Big loop to traverse thru document lines
+        for line in file_list:
+
+            # Loop to go thru each char (left -> right) & find first num.
+            for firstNum in line:
+                
+                if firstNum.isnumeric():
+                    a = int(firstNum)
+                    break
+
+            # Loop to go thru each char (right -> left) & find last num.
+            for lastNum in reversed(line):
+
+                if lastNum.isnumeric():
+                    b = int(lastNum)
+                    break
+
+            sumList.append(((a*10) + b))
+
+            # At the end of everything, clear list for next iteration of line.
+            indvCharList.clear()
+
+        for numEntry in sumList:
+            total += numEntry
+
+        print(total)
+
+main()
